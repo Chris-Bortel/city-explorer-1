accc81af2b3040545d70f26f804625d82decc299
Davee Sok <daveesok@gmail.com>
2020-06-21 17:18:17 -0700

    updated date format

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
-'use strict';
+"use strict";
 
 // import dotenv, express, cors
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
 
 // Anything from .env file shows up here
 const PORT = process.env.PORT;
@@ -13,10 +13,9 @@ const app = express();
 
 app.use(cors());
 
-
 //  Location
-app.get('/location', (request, response) => {
-  let data = require('./data/location.json');
+app.get("/location", (request, response) => {
+  let data = require("./data/location.json");
   let actualData = new Location(data[0], request.query.city);
   response.status(200).json(actualData);
 });
@@ -29,10 +28,10 @@ function Location(obj, city) {
 }
 
 // Weather
-app.get('/weather', (request, response) => {
-  let weatherData = require('./data/weather.json');
+app.get("/weather", (request, response) => {
+  let weatherData = require("./data/weather.json");
   let weekPrediction = [];
-  weatherData.data.forEach(day => {
+  weatherData.data.forEach((day) => {
     let forecast = new Forecast(day);
     weekPrediction.push(forecast);
   });
@@ -41,14 +40,13 @@ app.get('/weather', (request, response) => {
 
 function Forecast(obj) {
   this.forecast = obj.weather.description;
-  this.time = obj.datetime;
+  this.time = new Date(obj.datetime).toDateString();
 }
 
-
-app.use('*', (request, response) => {
+app.use("*", (request, response) => {
   let errorMsg = {
     status: 500,
-    responseText: 'Sorry, something went wrong'
+    responseText: "Sorry, something went wrong",
   };
   response.status(500).json(errorMsg);
 });
@@ -62,5 +60,4 @@ app.use('*', (request, response) => {
 //   response.status(500).json(errorMsg);
 // });
 
-app.listen(PORT, () => console.log('Server is running on port ', PORT));
-
+app.listen(PORT, () => console.log("Server is running on port ", PORT));
