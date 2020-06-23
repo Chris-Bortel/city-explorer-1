95e8a86a0c26207ff1ec57cc94f229f63e66b8f0
Davee Sok <daveesok@gmail.com>
2020-06-23 16:06:01 -0700

    updated trails

--- server.js
+++ server.js
@@ -21,10 +21,12 @@ app.use(cors());
 app.listen(PORT, () => console.log('Server is running on port ', PORT));
 
 app.get('/', (request, response) => {
-  response.send('Hello World again. Initial route');
+  response.json('Hello World again. Initial route');
 });
 
-//  Location
+
+
+///////////  Location  ////////////////
 app.get('/location', (request, response) => {
   const API = `https://us1.locationiq.com/v1/search.php?key=${process.env.GEOCODE_API_KEY}&q=${request.query.city}&format=json`;
 
@@ -32,10 +34,10 @@ app.get('/location', (request, response) => {
     .get(API)
     .then((data) => {
       let location = new Location(data.body[0], request.query.city);
-      response.status(200).send(location);
+      response.status(200).json(location);
     })
     .catch(() => {
-      response.status(500).send('Something went wrong in Location Route');
+      response.status(500).json('Something went wrong in Location Route');
     });
 });
 
@@ -47,21 +49,20 @@ function Location(obj, city) {
 }
 
 
-// Weather
+
+//////////   Weather   ///////////////
 app.get('/weather', (request, response) => {
   const API = `https://api.weatherbit.io/v2.0/forecast/daily?&lat=${request.query.latitude}&lon=${request.query.longitude}&key=${process.env.WEATHER_API_KEY}`;
 
   superagent
     .get(API)
     .then((data) => {
-      let weatherData = JSON.parse(data.text).data;
-      let weatherForcast = weatherData.map((day) => {
-        return new Forecast(day);
-      });
-      response.status(200).send(weatherForcast);
+      let weatherDataArr = JSON.parse(data.text).data;
+      let weatherForcast = weatherDataArr.map((day) => new Forecast(day));
+      response.status(200).json(weatherForcast);
     })
     .catch(() => {
-      response.status(500).send('Something went wrong in Weather Route');
+      response.status(500).json('Something went wrong in Weather Route');
     });
 });
 
@@ -72,7 +73,34 @@ function Forecast(obj) {
 
 
 
+//////////   Trails   ////////////////
+app.get('/trails', (request, response) => {
+  const API = `https://www.hikingproject.com/data/get-trails?lat=${request.query.latitude}&lon=${request.query.longitude}&key=${process.env.TRAIL_API_KEY}`;
 
+  superagent
+    .get(API)
+    .then((data) => {
+      let trailsDataArr = JSON.parse(data.text).trails;
+      let trailsRefactored = trailsDataArr.map(trail => new Trails(trail));
+      response.status(200).json(trailsRefactored);
+    })
+    .catch(() => {
+      response.status(500).json('Something wrong with Trails Route');
+    });
+});
+
+function Trails(obj) {
+  this.name = obj.name;
+  this.location = obj.location;
+  this.length = obj.length;
+  this.stars = obj.stars;
+  this.star_votes = obj.starVotes;
+  this.summary = obj.summary;
+  this.trail_url = obj.url;
+  this.conditions = obj.conditionDetails;
+  this.condition_date = obj.conditionDate.slice(0, obj.conditionDate.indexOf(' '));
+  this.condition_time = obj.conditionDate.slice(obj.conditionDate.indexOf(' '));
+}
 
 
 app.use('*', (request, response) => {
